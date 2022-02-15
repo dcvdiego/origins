58196dc2dda55308e7f6b45c008fa1235fd8a87c
Diego Sergio Chuman Perez <diegochuman@gmail.com>
2022-02-15 10:19:08 +0000

    fixed index links

--- index.php
+++ index.php
@@ -1,7 +1,10 @@
 <html>
-    <head></head>
-    <body>
-    <button onclick="location.href = 'containers/Origins/IBISFUN/index.php';" id="formbutton">IBISFUN</button>
-    <button onclick="location.href = 'containers/Origins/VMent/index.html';" id="formbutton">VMent</button>
-    </body>
-    </html>
+
+<head></head>
+
+<body>
+    <button onclick="location.href = 'containers/IBISFUN/index.php';" id="formbutton">IBISFUN</button>
+    <button onclick="location.href = 'containers/VMent/index.html';" id="formbutton">VMent</button>
+</body>
+
+</html>
