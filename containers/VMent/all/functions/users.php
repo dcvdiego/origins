e2331f89e2530ca9bf42bb36ed12566b0dbbc66e
dcvdiego <diegochuman@gmail.com>
2022-02-16 13:04:35 +0000

    fixed white page issue

--- containers/VMent/all/functions/users.php
+++ containers/VMent/all/functions/users.php
@@ -1,17 +1,5 @@
 <?php
 include $_SERVER['DOCUMENT_ROOT'] . "/origins/containers/VMent/all/database/connect.php";
-function mysqli_result($res, $row = 0, $col = 0)
-{
-	$numrows = mysqli_num_rows($res);
-	if ($numrows && $row <= ($numrows - 1) && $row >= 0) {
-		mysqli_data_seek($res, $row);
-		$resrow = (is_numeric($col)) ? mysqli_fetch_row($res) : mysqli_fetch_assoc($res);
-		if (isset($resrow[$col])) {
-			return $resrow[$col];
-		}
-	}
-	return false;
-}
 function doc_data($doc_id)
 {
 	global $connect;
